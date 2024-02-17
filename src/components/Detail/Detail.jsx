fcbc3da26280afe3b448bb17003063cc681e8c19
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-02-17 17:43:40 -0500

    Add styles on detail view

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,5 +1,6 @@
+import styles from "./Detail.module.css";
 import { useState, useEffect} from "react";
-import { Link, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import axios from "axios";
 
 export default function Detail(props){
@@ -16,23 +17,39 @@ export default function Detail(props){
            }           
         });
         return setCharacter({});
-    }, [detailId]);
+    }, [ detailId ]);
 
     return (
-    <div>   
-        <h2>
-            CHARACTER {detailId}  
-        </h2>      
+    <div className = {styles.detailContainer}>   
+        <div className = {styles.imgContainer}>
+            <img src = "/mortyDetail.png"
+                 alt = "Error img" 
+            />            
+        </div>    
+
+        <div className = {styles.characterContainer}>
+            <div className = {styles.nameImg}>
+                <h3> CHARACTER {detailId}  </h3>
+                {character.image  && 
+                    <img src = { character.image } 
+                        alt = "Error img"                     
+                    />
+                }  
+            </div>
+            
+            <div className = {styles.informationContainer}>
+                {character.name   && <h3>Name   : {character.name}       </h3>}
+                {character.status && <h3>Status : {character.status}     </h3>}
+                {character.species&& <h3>Species: {character.species}    </h3>}
+                {character.gender && <h3>Gender : {character.gender}     </h3>}
+                {character.origin && <h3>Origin : {character.origin.name}</h3>}
+            </div>
+        </div>
         
-        {character.name   && <h3>Name   : {character.name}       </h3>}
-        {character.status && <h3>Status : {character.status}     </h3>}
-        {character.species&& <h3>Species: {character.species}    </h3>}
-        {character.gender && <h3>Gender : {character.gender}     </h3>}
-        {character.origin && <h3>Origin : {character.origin.name}</h3>}
-        {character.image  && <img src   = {character.image} 
-                                alt   = "imagen no cargó" 
-                                width ="180px"
-                            />
-        }     
+        <div className = {styles.imgContainer}>
+            <img src = "/rickDetail.png"
+                 alt = "Error img" 
+            />            
+        </div>       
     </div>);
 }
