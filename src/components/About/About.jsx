4d515c886f525d5f26ef90e49c1c196a11866215
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-02-19 12:07:35 -0500

    Add styles on about creator view

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,30 @@
 import foto from '../../Imagenes/Foto.PNG'
+import styles from "./About.module.css"
 import { Link } from 'react-router-dom';
 
-export default function About (props){
+export default function About (){
     return(
-    <div>                
-        <div id="Imgn">
-            <img id  = "Foto" 
-                 src = {foto}
-                 style = {{width:"200px",
-                           border:"2px solid black"}}
-            />
-            <div id="PieImg">
-                <span> Desarrollo Web </span>        
-            </div>            
+    <div className = { styles.containerGeneral }>                
+        <div className = { styles.ImgnPicture }>
+            <h3> Ana María Orozco </h3>        
+            <img src = {foto}/>
         </div> 
 
-        <p>Ana María Orozco</p>  
-        <p>26 años</p>
-        <p>Colombia</p>
-        <p>Ingeniera Biomédica</p>
-        <p>Universidad Autónoma de Manizales</p>
+        <div className = { styles.information }>
+            <div className = { styles.containerA }>
+                <h3>Ingeniera Biomédica</h3>
+                <h3>Universidad Autónoma de Manizales</h3>
+                <h3>Colombia</h3>
+                <h3>Developer Full Stack</h3>
+                <h3>BootCamp Henry</h3>
+                <h3>Argentina</h3>
+            </div>
+            <div className = { styles.containerB }>
+                <img src = "/IESOG.png" alt = "Error" />
+                <img src = "/SENA.png"  alt = "Error" />
+                <img src = "/UAM.png"   alt = "Error" />
+                <img src = "/Henry.png" alt = "Error" />
+            </div>
+        </div>
     </div>);
 }
